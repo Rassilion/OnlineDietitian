660337cb4e5eaea7db9ad11a24be199b200523d4
erens <erens@MONSTERA5>
2016-11-30 17:58:53 +0300

    Add project files.

---
+++ OnlineDietitian/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineDietitian.Startup))]
+namespace OnlineDietitian
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
