a4c49fae5fe908008f455cc68f153e74307909a9
Deniz <denizgezgin365@gmail.com>
2016-12-07 21:12:10 +0300

    add payment entitiy

---
+++ Entity/PaymentE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class PaymentE
+    {
+        public int PayID { get; set; }
+        public int DietitianID { get; set; }
+        public int UserID { get; set; }
+        public DateTime PayDate { get; set; }
+        public string Fee { get; set; }
+        public string BankCard { get; set; }
+    }
+}
