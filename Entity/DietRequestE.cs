0aba97d1a1aeb48c896c03930f3e94262aae2321
hannibalecter <erensozen94@hotmail.com>
2016-12-07 21:21:45 +0300

    DietRequest entitiy add

---
+++ Entity/DietRequestE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class DietRequestE
+    {
+        public int RequestID { get; set; }
+        public string RequestContent { get; set; }
+        public bool Health { get; set; }
+        public bool Fitness { get; set; }
+        public bool WeightGain { get; set; }
+        public bool WeightLoss { get; set; }
+
+
+
+    }
+}
