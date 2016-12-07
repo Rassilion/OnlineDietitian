e4623aa5becef3fcfae28d68b0b26402262be23b
Deniz <denizgezgin365@gmail.com>
2016-12-07 21:20:38 +0300

    add diet entity

---
+++ Entity/DietE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class DietE
+    {
+        public int DietID { get; set; }
+        public int DietitianID { get; set; }
+        public int UserID { get; set; }
+        public int DietRequestID { get; set; }
+        public string Fee { get; set; }
+        public string BankCard { get; set; }
+    }
+}
