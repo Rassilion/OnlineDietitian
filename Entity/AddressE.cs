d1d3d63f5e2581cabfcad44bd2cf219b81f5a523
Deniz <denizgezgin365@gmail.com>
2016-12-07 20:56:18 +0300

    add address entity

---
+++ Entity/AddressE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class AddressE
+    {
+        public int AddressID { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string Street { get; set; }
+        public string Apartment { get; set; }
+
+
+    }
+}
