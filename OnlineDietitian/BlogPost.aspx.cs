29e499f7892e73c25a9ffe21a86ad70ae8796b6c
Deniz <denizgezgin365@gmail.com>
2016-12-08 16:39:52 +0300

    blog post page

---
+++ OnlineDietitian/BlogPost.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineDietitian
+{
+    public partial class BlogPost : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            string blogpostid = Page.RouteData.Values["id"] as string;
+            Label1.Text = blogpostid;
+        }
+    }
+}
