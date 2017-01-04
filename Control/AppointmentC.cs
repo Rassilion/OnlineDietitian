a90b3e7b4d9b4feca4487b01670b9eeefd701943
Deniz <denizgezgin365@gmail.com>
2017-01-04 15:45:41 +0300

    delete controls

---
+++ Control/AppointmentC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entity;
+
+namespace Control
+{
+    public class AppointmentC
+    {
+        public static void DeleteAppointment(int id)
+        {
+
+            SqlCommand com = new SqlCommand("DeleteAppointment", Connection.Con); // Prodecure
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.Add(new SqlParameter("@id", id));
+
+            if (com.Connection.State == ConnectionState.Closed)
+            {
+                com.Connection.Open();
+            }
+            SqlDataReader rd = com.ExecuteReader();
+
+
+            com.Dispose();
+            com.Connection.Close();
+
+        }
+    }
+}
---
+++ Control/MedicalReportC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entity;
+
+namespace Control
+{
+    public class MedicalReportC
+    {
+        public static void DeleteMedicalReport(int id)
+        {
+
+            SqlCommand com = new SqlCommand("DeleteMedicalReport", Connection.Con); // Prodecure
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.Add(new SqlParameter("@id", id));
+
+            if (com.Connection.State == ConnectionState.Closed)
+            {
+                com.Connection.Open();
+            }
+            SqlDataReader rd = com.ExecuteReader();
+
+
+            com.Dispose();
+            com.Connection.Close();
+
+        }
+    }
+}
