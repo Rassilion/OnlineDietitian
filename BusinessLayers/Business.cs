983131deb46881aba6f1bf9e472d5f9adce61b24
Deniz <denizgezgin365@gmail.com>
2017-01-07 14:44:36 +0300

    log enttiy

--- BusinessLayers/Business.cs
+++ BusinessLayers/Business.cs
@@ -10,6 +10,53 @@ namespace BusinessLayers
 {
     public class Business
     {
+
+        //log
+        public static void logLoginU(string userid)
+        {
+            addLog("LoginU",userid);
+        }
+        public static void logLoginD(string userid)
+        {
+            addLog("LoginD", userid);
+        }
+        public static void logLoginFailU(string email)
+        {
+            addLog("LoginFailU", email);
+        }
+        public static void logLoginFailD(string email)
+        {
+            addLog("LoginFailD", email);
+        }
+        public static void logUpdate(string table,string userID, string id)
+        {
+            addLog("Update"+table," userID: "+userID + " update id: " + id);
+        }
+
+        public static void logInsert(string table, string userID, string id)
+        {
+            addLog("Insert" + table," userID: " + userID + " insert id: " + id);
+        }
+
+        public static void logDelete(string table, string userID, string id)
+        {
+            addLog("Delete" + table," userID: " + userID +" deleted id: "+id);
+        }
+
+        public static void logDiet(string userid, string id)
+        {
+            addLog("CreateDiet", "userID: "+userid+" DietID: "+id);
+        }
+        public static void logDietFollowUp(string userid, string id)
+        {
+            addLog("CreateDietFollowUp", "diettianID: " + userid + " DietID: " + id);
+        }
+
+        protected static void addLog(string type,string message)
+        {
+
+        }
+
         // BlogPost start
         public static List<BlogPostE> getBlogPosts()
         {
